0bdd40f2a3c34771f2a75a4c0ae0e86865019995
uprogr <uprogr@gmail.com>
2023-06-14 12:37:24 +0800

    zuu service nemeh

--- whoisBEv10/whoisBEv10/urls.py
+++ whoisBEv10/whoisBEv10/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from app1 import user
+from app1 import user, basic
+
 urlpatterns = [    
     #    user servoces
     path('userList/', user.userListView, name='userListView'),
+    path('zuu/', basic.zuu, name='zuuListView'),
     ######################################################
 ]
