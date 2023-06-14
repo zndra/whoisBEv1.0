168acad7025e915b45dbbd3ab35136a9c8460a34
uprogr <uprogr@gmail.com>
2023-06-14 12:32:22 +0800

    ZUU TEST

--- whoisBEv10/app1/basic.py
+++ whoisBEv10/app1/basic.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
 # from django.http import JsonResponse
 import json
+from  whoisBEv10.settings import *
 
 # Create your views here.
 def zuu(request):
     response = {}
     response["hariu"] = "hello"
     response["surguuli"] = "Mandakh"
+    response["usuhuu"] = mandakhHash('usuhuu')
     responseJSON = json.dumps(response)
     return HttpResponse(responseJSON,content_type="application/json")
     # return JsonResponse({'response': responseJSON})
