00f48a1863ebf8b09e9b9bbc426a122c408b6a1b
Ganzorig Ulziibat <ganzoo@mandakh.edu.mn>
2023-06-15 15:39:20 +0800

    login service

--- whoisBEv10/app1/user.py
+++ whoisBEv10/app1/user.py
@@ -14,24 +14,36 @@ def userListView(request):
 
     userCursor.close()        
     disconnectDB(myCon)
-    # try:
-    #     myCon = connectDB()
-    #     userCursor = myCon.cursor()
-    #     # Ganzo - query yanzlah
-    #     userCursor.execute("select * from 'user'"
-    #                         "ORDER BY id ASC")
-    #     columns = userCursor.description
-    #     response = [{columns[index][0]:column for index, column in enumerate(
-    #         value)} for value in aimagCursor.fetchall()]
-
-    #     userCursor.close()        
-    # except Exception as e:
-    #     response = {}
-    #     response["hariu"] = "hello"
-    #     response["surguuli"] = "Mandakh"
-    # finally:
-    #     disconnectDB(myCon)
 
     responseJSON = json.dumps(response)
     return HttpResponse(responseJSON,content_type="application/json")
-    
+
+def userLoginView(request):
+    # reqData = request.body
+    jsons = json.loads(request.body)
+    myName = jsons['name']
+    myPass = jsons['pass']
+
+    myCon = connectDB()
+    userCursor = myCon.cursor()
+    userCursor.execute('select count(id) as too from "user"'
+                         ' where "userName"=\''+ myName+ '\' '
+                         ' and "pass"=\''+ myPass+ '\''
+                         )    
+    columns = userCursor.description
+    response = [{columns[index][0]:column for index, column in enumerate(
+        value)} for value in userCursor.fetchall()]    
+    userCursor.close()        
+    disconnectDB(myCon)
+
+    responseCode = 0
+    responseText = 'Буруу нэр/нууц үг'
+    if response[0]['too'] != 0:
+        responseCode = 200
+        responseText = 'Зөв нэр/нууц үг байна хөгшөөн'
+    resp = {}
+    resp["responseCode"] = responseCode
+    resp["responseText"] = responseText
+
+
+    return  HttpResponse(json.dumps(resp),content_type="application/json")    
--- whoisBEv10/whoisBEv10/urls.py
+++ whoisBEv10/whoisBEv10/urls.py
@@ -4,6 +4,7 @@ from app1 import user, basic
 urlpatterns = [    
     #    user servoces
     path('userList/', user.userListView, name='userListView'),
+    path('userLogin/', user.userLoginView, name='userLoginView'),
     path('zuu/', basic.zuu, name='zuuListView'),
     ######################################################
 ]
