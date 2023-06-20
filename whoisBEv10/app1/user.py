b88fd901e2b835b56a926bb79f988acd5ff09bf9
uprogr <uprogr@gmail.com>
2023-06-20 18:01:17 +0800

    zzzzzzz

--- whoisBEv10/app1/user.py
+++ whoisBEv10/app1/user.py
@@ -51,12 +51,14 @@ def userLoginView(request):
 
     responseCode = 521  # login error
     responseText = 'Буруу нэр/нууц үг'
-    # if response and response[0]['too'] != 0:
-    #     responseCode = 200
-    #     responseText = 'Зөв нэр/нууц үг байна хөгшөөн'
+    if response[0]:
+        responseCode = 200
+        responseText = 'Зөв нэр/нууц үг байна хөгшөөн'
+        responseData = response[0]
     resp = {}
     resp["responseCode"] = responseCode
     resp["responseText"] = responseText
+    resp["userData"] = responseData
 
     return HttpResponse(json.dumps(resp), content_type="application/json")
 
