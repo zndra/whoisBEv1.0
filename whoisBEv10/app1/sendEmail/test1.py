349ca352870d14c515ed74dd54655f05ad8f7f90
uprogr <uprogr@gmail.com>
2023-06-21 03:34:03 +0800

    gitignore asd

--- whoisBEv10/app1/sendEmail/test1.py
+++ whoisBEv10/app1/sendEmail/test1.py
@@ -1,8 +1,38 @@
-def runQuery(query):
-    print(query)
+import psycopg2 
+pgDbName   = "dbwhois"
+pgUser     = "uwhois"
+pgHost     = "202.131.254.138"
+pgPassword = "whoispass"
+pgPort     = "5938"
 
-id=10
-pa="asd"
+def connectDB():
+    con = psycopg2.connect(
+        dbname   = pgDbName,
+        user     = pgUser,
+        host     = pgHost,
+        password = pgPassword,
+        port     = pgPort,
+    )
+    return con
 
-runQuery("UPDATE user SET pass=%s WHERE id = %s" % (pa,id))
+def disconnectDB(con):
+    if(con):
+        con.close()
+        # is email overlaped
+def emailExists(email):
+    myCon = connectDB()
+    userCursor = myCon.cursor()
+    userCursor.execute('SELECT COUNT(*) FROM "f_user" WHERE "email" = %s', (email,))
+    result = userCursor.fetchone()
+    userCursor.close()
+    disconnectDB(myCon)
+    return result[0] > 0
 
+
+myCon = connectDB()
+
+# print(myCon)
+
+print(emailExists('uproagr@gmail.com'))
+
+disconnectDB(myCon)
