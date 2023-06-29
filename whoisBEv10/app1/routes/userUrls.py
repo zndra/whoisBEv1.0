c8fa4210f18d00e27448480a10b4a042ab755079
Tuugii <85017432+Tuugiixtyl@users.noreply.github.com>
2023-06-29 15:34:34 +0800

    routes structure changes & gitignore

---
+++ whoisBEv10/app1/routes/userUrls.py
@@ -0,0 +1,41 @@
+from app1.user   import *
+from django.urls import path
+
+app_name = "userServices"
+
+urlpatterns = [    
+    #    user services
+    path('userLogin/',             userLoginView,      name='userLoginView'),
+    path('userRegister/',          userRegisterView,   name='userRegisterView'),
+    path('forgetPass/',            forgetPass,         name='forgetPass'),
+    path('changePass/',            changePass,         name='changePass'),
+    path('resetPassword/',         resetPasswordView,  name='resetPasswordView'),
+    path('userNemelt/',            userNemeltGet,      name='userNemeltMedeelelView'),
+    path('verifyCode/',            verifyCodeView,     name='verifyCodeView'),
+    path('userNemeltUp/',          userNemeltUpdate,   name='userNemeltUpdate'),
+    path('verifyEmail/<str:otp>/', verifyEmailView,    name='verifyEmailView'),
+    path('userNemeltIns/',         userNemeltInsert,   name='userNemeltInsert'),
+    path('userInfoShow/',          userInfoShowView,   name='userInfoShow'),
+    path('userInfoUpdate/',        userInfoUpdateView, name='uuserInfoUpdateView'),
+    path('userEduUp/',             userEduUp,          name='userEduUp'),
+    path('userFamily/',            userFamilyGet,      name= 'userFamilyGet'),
+    ################################################################################
+    path('userSocial/',            userSocial,         name='userSocial'),
+    path('userSocialUp/',          userSocialUp,       name='userSocialup'),
+    path('userSocialIn/',          userSocialIn,       name='userSocialIn'),
+    ################################################################################
+    path('userEduGet/',            userEduGet,         name='userEduGet'),
+    path('userEduInsert/',         userEduInsert,      name='usereduInsert'),
+    path('userTurshlaga/' ,        userTurshlaga,      name='userTurshlaga'   ),
+    path('userTurshlagaUp/' ,      userTurshlagaUp,    name='userTurshlagaUp'   ),
+    path('userTurshlagaIn/' ,      userTurshlagaIn,    name='userTurshlagaIn'   ),
+    path('getUserSkill/' ,         getUserSkillView,   name='getUserSkillView'   ),
+    path('setUserSkill/' ,         setUserSkillView,   name='setUserSkillView'   ),
+    ######################################################
+
+    path('userList/',              userListView,       name='userListView'),
+    ######################################################
+    path('getTransactionLog/', getTransactionLog, name="getTransactionLog"),
+    path('makeTransaction/', makeTransaction, name="makeTransaction"),
+    ################################################################################
+]
--- whoisBEv10/whoisBEv10/urls.py
+++ whoisBEv10/whoisBEv10/urls.py
@@ -1,41 +1,11 @@
 from app1        import user, testViews
-from django.urls import path
+from django.urls import path, include
 urlpatterns = [    
-    #    user servoces
-    path('userLogin/',             user.userLoginView,      name='userLoginView'),
-    path('userRegister/',          user.userRegisterView,   name='userRegisterView'),
-    path('forgetPass/',            user.forgetPass,         name='forgetPass'),
-    path('changePass/',            user.changePass,         name='changePass'),
-    path('resetPassword/',         user.resetPasswordView,  name='resetPasswordView'),
-    path('userNemelt/',            user.userNemeltGet,      name='userNemeltMedeelelView'),
-    path('verifyCode/',            user.verifyCodeView,     name='verifyCodeView'),
-    path('userNemeltUp/',          user.userNemeltUpdate,   name='userNemeltUpdate'),
-    path('verifyEmail/<str:otp>/', user.verifyEmailView,    name='verifyEmailView'),
-    path('userNemeltIns/',         user.userNemeltInsert,   name='userNemeltInsert'),
-    path('userInfoShow/',          user.userInfoShowView,   name='userInfoShow'),
-    path('userInfoUpdate/',        user.userInfoUpdateView, name='uuserInfoUpdateView'),
-    path('userEduUp/',             user.userEduUp,          name='userEduUp'),
-    path('userFamily/',            user.userFamilyGet,      name= 'userFamilyGet'),
-    ################################################################################
-    path('userSocial/',            user.userSocial,         name='userSocial'),
-    path('userSocialUp/',          user.userSocialUp,       name='userSocialup'),
-    path('userSocialIn/',          user.userSocialIn,       name='userSocialIn'),
-    ################################################################################
-    path('userEduGet/',            user.userEduGet,         name='userEduGet'),
-    path('userEduInsert/',         user.userEduInsert,      name='usereduInsert'),
-    path('userTurshlaga/' ,        user.userTurshlaga,      name='userTurshlaga'   ),
-    path('userTurshlagaUp/' ,      user.userTurshlagaUp,    name='userTurshlagaUp'   ),
-    path('userTurshlagaIn/' ,      user.userTurshlagaIn,    name='userTurshlagaIn'   ),
-    path('getUserSkill/' ,         user.getUserSkillView,   name='getUserSkillView'   ),
-    path('setUserSkill/' ,         user.setUserSkillView,   name='setUserSkillView'   ),
-    ######################################################
+    #    user services
+    path("", include("app1.routes.userUrls", namespace='userServices')),
 
     # тестийн service-үүд. төслөө хverifyEmailийж дуусчаад устганаа.
-    path('userList/',              user.userListView,       name='userListView'),
     path('zuu/',                   testViews.zuu,           name='zuuListView'),
     path('tavFact/',               testViews.tavFactView,   name='tavFactListView'),
-    ######################################################
-    path('getTransactionLog/', user.getTransactionLog, name="getTransactionLog"),
-    path('makeTransaction/', user.makeTransaction, name="makeTransaction"),
     ################################################################################
 ]
