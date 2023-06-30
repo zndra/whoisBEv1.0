89eb80a69a9725b9fdf6c6f1849262936cf590bc
xusln <huslenotgonbayar0526@gmail.com>
2023-06-30 18:41:17 +0800

    jijijg

--- whoisBEv10/app1/user.py
+++ whoisBEv10/app1/user.py
@@ -807,48 +807,7 @@ def userEduGet(request):
         "eduData": {}  # Add an empty "data" field in the response
     }
     return HttpResponse(json.dumps(response), content_type="application/json")
-
-# def userEduGet(request):
-#     jsons = json.loads(request.body)
-#     user_id = jsons['user_id']
-#     if request.method == 'GET':
-#         myCon = connectDB()
-#         userCursor = myCon.cursor()
-#         userCursor.execute(
-#             'SELECT * FROM "f_userEdu" WHERE "user_id"= %s', (user_id,))
-#         user = userCursor.fetchone()
-#         if not user:
-#             response = {
-#                 "responseCode": 555,
-#                 "responseText": "Хэрэглэгч олдсонгүй"
-#             }
-#             userCursor.close()
-#             disconnectDB(myCon)
-#             return HttpResponse(json.dumps(response), content_type="application/json")
-#         elif user:
-#             userCursor.execute(
-#                 'SELECT * FROM "f_userEdu" WHERE "user_id"= %s', (user_id,))
-#             columns = [column[0] for column in userCursor.description]
-
-#             response = [
-#                 {columns[index]: column for index, column in enumerate(value)}
-#                 for value in userCursor.fetchall()
-#             ]
-#             userCursor.close()
-#             disconnectDB(myCon)
-
-#             responseJSON = json.dumps(
-#                 (response), cls=DjangoJSONEncoder, default=str)
-#             return HttpResponse(responseJSON, content_type="application/json")
-#         else:
-#             response = {
-#                 "responseCode": 551,
-#                 "responseText": "Баазын алдаа"
-#             }
-#             return HttpResponse(json.dumps(response), content_type="application/json")
-################################################################################################
-
-
+######################################################################################
 def userSocial(request):
     jsons = json.loads(request.body)
     user_id = jsons['id']
